e1d41293d1be55ed72d3c255ffa77ff057212dd6
kcfreshz <kckc176@gmail.com>
2025-11-19 20:09:24 +0100

    adjusted the cors

--- backend/server.js
+++ backend/server.js
@@ -8,12 +8,20 @@ dotenv.config();
 
 const app = express();
 
+// app.use(
+//   cors({
+//     origin: process.env.CLIENT_URL || "http://localhost:5173",
+//     credentials: true,
+//   })
+// );
+
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: "https://frontend-xzra.onrender.com/",
     credentials: true,
   })
 );
+
 app.use(express.json());
 app.use(cookieParser());
 
